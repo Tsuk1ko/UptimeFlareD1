4ce90240b7096b4c74f41ca33a9c0d1adbccbcac
神代綺凛 <i@loli.best>
2024-07-07 00:08:46 +0800

    style: fix monitor list item style

--- components/MonitorList.tsx
+++ components/MonitorList.tsx
@@ -2,25 +2,28 @@ import { MonitorState, MonitorTarget } from '@/uptime.types'
 import { Card, Center, Divider } from '@mantine/core'
 import MonitorDetail from './MonitorDetail'
 
-export default function MonitorList({ monitors, state }: { monitors: any; state: MonitorState }) {
+export default function MonitorList({
+  monitors,
+  state,
+}: {
+  monitors: MonitorTarget[]
+  state: MonitorState
+}) {
   return (
     <Center>
       <Card
         shadow="sm"
         padding="lg"
         radius="md"
-        ml="xl"
-        mr="xl"
+        mx="xl"
         mt="xl"
         withBorder
         style={{ width: '865px' }}
       >
-        {monitors.map((monitor: MonitorTarget) => (
-          <div key={monitor.id}>
-            <Card.Section ml="xs" mr="xs">
-              <MonitorDetail monitor={monitor} state={state} />
-            </Card.Section>
-            <Divider />
+        {monitors.map((monitor, i) => (
+          <div key={monitor.id} style={{ margin: '0 10px' }}>
+            <MonitorDetail monitor={monitor} state={state} />
+            {i !== monitors.length - 1 && <Divider mt="xs" />}
           </div>
         ))}
       </Card>
