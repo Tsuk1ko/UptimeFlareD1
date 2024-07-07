5485170db2a7c76301040a8335c96282a82f9aa8
神代綺凛 <i@loli.best>
2024-07-07 15:25:20 +0800

    feat: split state to chunks to avoid exceeding d1 row size limit

--- util/state.ts
+++ util/state.ts
@@ -1,36 +1,45 @@
+interface StateRow {
+  idx: number
+  val: string
+}
+
+// D1 max row size 1MB, so we split the state into chunks of 900KB and store them in separate rows.
+const SPLIT_SIZE = 9e5
+
 export class UptimeFlareStateDb<T> {
-  private stateExist = false
+  private stateSize = 0
 
   constructor(private db: D1Database) {}
 
   async get(): Promise<T | null> {
-    const result = await this.db
-      .prepare("SELECT * FROM kv WHERE key = 'state'")
-      .first<{ key: 'state'; value: string }>()
+    const { results } = await this.db
+      .prepare('SELECT * FROM state ORDER BY idx ASC')
+      .all<StateRow>()
 
-    this.stateExist = !!result
+    this.stateSize = results.length
 
-    return result ? this.safeJsonParse(result.value) : null
+    return this.stateSize ? this.safeJsonParse(results.map(({ val }) => val).join('')) : null
   }
 
   async set(value: T) {
-    if (this.stateExist) {
-      await this.db
-        .prepare("UPDATE kv SET value = ? WHERE key = 'state'")
-        .bind(JSON.stringify(value))
-        .run()
-    } else {
-      await this.db
-        .prepare("INSERT INTO kv (key, value) VALUES ('state', ?)")
-        .bind(JSON.stringify(value))
-        .run()
-      this.stateExist = true
+    const chunks = []
+    for (let str = JSON.stringify(value); str.length; str = str.slice(SPLIT_SIZE)) {
+      chunks.push(str.slice(0, SPLIT_SIZE))
     }
+    for (const [i, chunk] of chunks.entries()) {
+      if (i < this.stateSize) {
+        await this.db.prepare('UPDATE state SET val = ? WHERE idx = ?').bind(chunk, i).run()
+      } else {
+        await this.db.prepare('INSERT INTO state (idx, val) VALUES (?, ?)').bind(i, chunk).run()
+      }
+    }
+    this.stateSize = chunks.length
   }
 
   private safeJsonParse(str: string) {
     try {
-      return JSON.parse(str)
+      const val = JSON.parse(str)
+      return val && typeof val === 'object' ? val : null
     } catch {
       return null
     }
